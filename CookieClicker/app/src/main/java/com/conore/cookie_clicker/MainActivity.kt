3b6dd21e14b8e682e71a32902030a2f37f6b26e8
Conor Erickson <89137464+conorerickson@users.noreply.github.com>
2022-08-13 18:53:10 -0400

    Forgot to commit

--- CookieClicker/app/src/main/java/com/conore/cookie_clicker/MainActivity.kt
+++ CookieClicker/app/src/main/java/com/conore/cookie_clicker/MainActivity.kt
@@ -1,34 +1,71 @@
 package com.conore.cookie_clicker
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 private var currentScore = 0
 private var numOfClickers = 0
+private var scope = MainScope()
+private var job: Job? = null
+private var isFirstClick : Boolean = true
+private var cost : Int = 10
 
+    class MainActivity : AppCompatActivity() {
+        @SuppressLint("SetTextI18n")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            val tvAutoClickers: TextView = findViewById(R.id.tvAutoClickers)
+            val tvScore: TextView = findViewById(R.id.tvScore)
+            val ibCookie: ImageButton = findViewById(R.id.ibCookie)
+            val btnBuy: TextView = findViewById(R.id.btnBuy)
+            val price: TextView = findViewById(R.id.tvPrice)
 
-        val tvAutoClickers : TextView = findViewById(R.id.tvAutoClickers)
-        val tvScore : TextView = findViewById(R.id.tvScore)
-        val ibCookie : ImageButton = findViewById(R.id.ibCookie)
-        val btnBuy : TextView = findViewById(R.id.btnBuy)
+            fun autoClick() {
+                currentScore += numOfClickers
+                tvScore.text = "You have $currentScore Cookies!"
+            }
 
-        ibCookie.setOnClickListener {
-            currentScore ++
-            tvScore.text = "You have $currentScore Cookies!"
+            fun startUpdates() {
+                job = scope.launch {
+                    tvAutoClickers.text = "You have $numOfClickers Auto-Clickers!"
+                    price.text = "An Auto-Clicker costs $cost Cookies"
+                    while (true) {
+                        autoClick()
+                        delay(1000)
+                    }
+                }
+            }
 
-        }
-        btnBuy.setOnClickListener {
-            numOfClickers ++
-            tvAutoClickers.text = "You have $numOfClickers Auto-Clickers"
+            ibCookie.setOnClickListener {
+                currentScore++
+                tvScore.text = "You have $currentScore Cookies!"
+            }
+
+            btnBuy.setOnClickListener {
+                buy()
+                startUpdates()
             }
         }
-}
+
+        private fun buy() {
+            if(currentScore >= cost) {
+                currentScore -= cost
+                numOfClickers ++
+                cost *= 2
+            } else {
+                Toast.makeText(this, "Not Enough Cookies!", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+    }
